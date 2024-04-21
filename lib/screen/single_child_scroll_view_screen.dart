cc1cc8a554828ce29f46fa6efdb5b9b25f39dc0a
iammoong <ansrlwkd225@naver.com>
2024-04-21 21:30:19 +0900

    SingleChildScrollView 사용법

--- lib/screen/single_child_scroll_view_screen.dart
+++ lib/screen/single_child_scroll_view_screen.dart
@@ -1,31 +1,108 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:scrollable_widgets/const/colors.dart';
 import 'package:scrollable_widgets/layout/main_layout.dart';
 
 class SingleChildScrollViewScreen extends StatelessWidget {
-  const SingleChildScrollViewScreen({super.key});
+  final List<int> numbers = List.generate(
+    100,
+    (index) => index,
+  );
+
+  SingleChildScrollViewScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MainLayout(
       title: 'SingleChildScrollView',
-      body: SingleChildScrollView(
-        child: Column(
-          children: rainbowColors
-              .map(
-                (e) => renderContainer(
-                  color: e,
-                ),
-              )
-              .toList(),
-        ),
+      body: renderPerformance(),
+    );
+  }
+
+  // 1. 기본 렌더링
+  Widget rendorSimple() {
+    return SingleChildScrollView(
+      child: Column(
+        children: rainbowColors
+            .map(
+              (e) => renderContainer(
+                color: e,
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+
+  //2. 화면을 넘어가지 않아도 스크롤 되게하기
+  Widget renderAlwaysScroll() {
+    return SingleChildScrollView(
+      physics: AlwaysScrollableScrollPhysics(),
+      child: Column(
+        children: [
+          renderContainer(color: Colors.black),
+        ],
+      ),
+    );
+  }
+
+  // 3. 위젯이 잘리지 않게 하기
+  Widget renderClip() {
+    return SingleChildScrollView(
+      clipBehavior: Clip.antiAlias,
+      physics: AlwaysScrollableScrollPhysics(),
+      child: Column(
+        children: [
+          renderContainer(color: Colors.black),
+        ],
+      ),
+    );
+  }
+
+  //4. 여러가지 physics 정리
+  Widget renderPhysics() {
+    return SingleChildScrollView(
+      // NeverScrollableScrollPhysics : 스크롤 안되게 하기
+      // AlwaysScrollableScrollPhysics : 스크롤 되게 함
+      // BouncingScrollPhysics : iOS 스타일로 바꾸는 스크롤
+      // ClampingScrollPhysics : 안드로이드 스타일로 바꾸는 스크롤
+      physics: ClampingScrollPhysics(),
+      child: Column(
+        children: rainbowColors
+            .map(
+              (e) => renderContainer(
+                color: e,
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+
+  //5. SingleChildScrollView 퍼포먼스
+  Widget renderPerformance(){
+    return SingleChildScrollView(
+      child: Column(
+        children: numbers
+            .map(
+              (e) => renderContainer(
+            color: rainbowColors[e % rainbowColors.length],
+            index: e,
+          ),
+        )
+            .toList(),
       ),
     );
   }
 
   Widget renderContainer({
     required Color color,
+    int? index,
   }) {
+    if(index != null) {
+      print(index);
+    }
     return Container(
       height: 300,
       color: color,
