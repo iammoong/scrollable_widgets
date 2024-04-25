c0d9d886404a9e862abe37ef73f5953965d3fd5f
iammoong <ansrlwkd225@naver.com>
2024-04-25 21:18:21 +0900

    GridView 예제2

--- lib/screen/grid_view_screen.dart
+++ lib/screen/grid_view_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:scrollable_widgets/const/colors.dart';
 import 'package:scrollable_widgets/layout/main_layout.dart';
@@ -10,11 +11,47 @@ class GridViewScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return MainLayout(
         title: 'GridViewScreen',
-        body:GridView.count(
-          crossAxisCount: 2,
-          children: numbers.map((e) => renderContainer(
-              color: rainbowColors[e % rainbowColors.length], index: e,),).toList()
-        ),
+        body: renderMaxExtent(),
+    );
+  }
+
+  //1. 한번에 다 그려주는 리스트
+  Widget renderCount() {
+    return GridView.count(
+        crossAxisCount: 2,
+        crossAxisSpacing: 12.0,
+        mainAxisSpacing: 12.0,
+        children: numbers.map((e) => renderContainer(
+          color: rainbowColors[e % rainbowColors.length], index: e,),).toList()
+    );
+  }
+
+  //2. 보이는 것만 그려주는 리스트
+  Widget renderBuilderCrossAxisCount(){
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 12.0,
+        mainAxisSpacing: 12.0,
+      ),
+      itemBuilder: (context, index){
+        return renderContainer(
+            color: rainbowColors[index % rainbowColors.length], index: index
+        );
+      },
+    );
+  }
+  //3. 최대 사이즈를 그려주는 리스트
+  Widget renderMaxExtent(){
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 300,
+      ),
+      itemBuilder: (context, index){
+        return renderContainer(
+            color: rainbowColors[index % rainbowColors.length], index: index
+        );
+      },
     );
   }
 
